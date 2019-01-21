93074b98fbf130c009e3920ccaf9b56f5641be98
MyPC <a@b.com>
2019-01-21 22:04:23 +0700

    fix docs

--- FFMpeg.php
+++ FFMpeg.php
@@ -2,12 +2,14 @@
 
 namespace phuong17889\ffmpeg;
 
+use Alchemy\BinaryDriver\ConfigurationInterface;
 use FFMpeg\Driver\FFMpegDriver;
 use FFMpeg\Exception\InvalidArgumentException;
 use FFMpeg\Exception\RuntimeException;
 use FFMpeg\FFProbe;
 use FFMpeg\Media\Audio;
 use phuong17889\ffmpeg\media\Video;
+use Psr\Log\LoggerInterface;
 
 class FFMpeg extends \FFMpeg\FFMpeg {
 
@@ -16,6 +18,34 @@ class FFMpeg extends \FFMpeg\FFMpeg {
 
 	/** @var FFProbe */
 	private $ffprobe;
+
+	/**
+	 * FFMpeg constructor.
+	 *
+	 * @param FFMpegDriver $ffmpeg
+	 * @param FFProbe      $ffprobe
+	 */
+	public function __construct(FFMpegDriver $ffmpeg, FFProbe $ffprobe) {
+		parent::__construct($ffmpeg, $ffprobe);
+		$this->driver  = $ffmpeg;
+		$this->ffprobe = $ffprobe;
+	}
+
+	/**
+	 * Creates a new FFMpeg instance.
+	 *
+	 * @param array|ConfigurationInterface $configuration
+	 * @param LoggerInterface              $logger
+	 * @param FFProbe                      $probe
+	 *
+	 * @return FFMpeg
+	 */
+	public static function create($configuration = array(), LoggerInterface $logger = null, FFProbe $probe = null) {
+		if (null === $probe) {
+			$probe = FFProbe::create($configuration, $logger, null);
+		}
+		return new static(FFMpegDriver::create($logger, $configuration), $probe);
+	}
 
 	/**
 	 * Opens a file in order to be processed.
@@ -36,17 +66,5 @@ class FFMpeg extends \FFMpeg\FFMpeg {
 			return new Audio($pathfile, $this->driver, $this->ffprobe);
 		}
 		throw new InvalidArgumentException('Unable to detect file format, only audio and video supported');
-	}
-
-	/**
-	 * FFMpeg constructor.
-	 *
-	 * @param FFMpegDriver $ffmpeg
-	 * @param FFProbe      $ffprobe
-	 */
-	public function __construct(FFMpegDriver $ffmpeg, FFProbe $ffprobe) {
-		parent::__construct($ffmpeg, $ffprobe);
-		$this->driver  = $ffmpeg;
-		$this->ffprobe = $ffprobe;
 	}
 }
